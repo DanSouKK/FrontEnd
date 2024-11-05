cfcda168178ce5d62a42c4475097ac2b18c0c18f
DanSouKK <dansouzaofficial@gmail.com>
2024-11-04 21:52:53 -0300

    Aula 5 html

---
+++ Daniel html Aula 5/exemplos2/main.js
@@ -0,0 +1,30 @@
+//alert("Ola")
+console.log("Ola, como vai?")
+
+var my_var;
+my_var = "Ola, pessoal";
+//alert(my_var);
+my_var = 5;
+//alert(my_var);
+my_var = [1, 10, "Ola", true];
+//alert(my_var);
+
+var variavel1 = "Ola mundo";
+//alert(variavel1);
+
+var variavel2 = 123;
+console.log(variavel2);
+let exemplo3 = "Eu tambem sou uma variavel";
+//alert(exemplo3);
+
+//incremento no js ++ --
+// basicos < <= > >=
+// igualdade == !=
+//identidade === !=== 
+//propriedade in
+//and: && OR: || Not: ~
+
+
+document.write("Olá mundo") //document aparece no body do html
+document.write("<h1>Eu sou um titulo escrito no js</h1>")
+document.write("<title> titulo da pagina </title>")// titulo da pagina direto pelo js
